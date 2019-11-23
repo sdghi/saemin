9016eed6f779617d57ac948df6cdc88ec2e250c3
frzrbox <david@freezerbox.dev>
2019-11-22 15:26:13 -1000

    adds css reset

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,18 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html {
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+}
+
+body {
+  margin: 0;
+  padding: 0;
 }
 </style>
