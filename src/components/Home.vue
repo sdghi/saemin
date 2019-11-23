184b5831b72bbdc77bab69b5221532dedbf83909
frzrbox <david@freezerbox.dev>
2019-11-22 15:49:31 -1000

    adds subtitle to the hero section

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,7 @@
 <template>
     <div id="app-container">
-        <h1 class="home-title" :class="{'quiz-started': quizStarted}">This is the home page</h1>
+        <h1 class="home-title" :class="{'quiz-started': quizStarted}">SDG Saemin Generator</h1>
+        <h3 v-if="!quizStarted" class="home-subtitle">What kind of ramen are you? Take the quiz to find out.</h3>
         <button v-if="!quizStarted" class="start-quiz-btn" @click="quizStarted = true">click to begin</button>
         <Quiz :quizStarted="quizStarted"/>
     </div>
@@ -23,6 +24,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "@/scss/_variables";
+
 #app-container {
   min-height: 100vh;
   width: 100%;
@@ -32,9 +35,29 @@ export default {
   position: relative;
   top: 2.5%;
   text-align: center;
-  font-size: 2vw;
+  font-size: 1.3rem;
   transition: all 0.3s ease-out;
-  transform: translateY(40vh) scale(3);
+  transform: translateY(40vh) scale(1.5);
+}
+
+.home-subtitle {
+  position: relative;
+  text-align: center;
+  top: 45vh;
+  font-size: 2rem;
+  width: 80%;
+  margin: 0 auto;
+  font-size: 1.3rem;
+}
+
+@media (min-width: $breakpoint-small) {
+  .home-title {
+    transform: translateY(40vh) scale(3.5);
+  }
+
+  .home-subtitle {
+    font-size: 2rem;
+  }
 }
 
 .home-title.quiz-started {
