982bf570d8b3898144ec699a17b1e6e100558d11
frzrbox <david@freezerbox.dev>
2019-11-25 15:40:26 -1000

    fixes background issue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -31,7 +31,12 @@ export default {
 @import "@/scss/partials/_variables";
 
 #app-container {
+  position: absolute;
+  top: 0;
+  left: 0;
   width: 100%;
+  min-height: 100vh;
+  background: $accent-color;
 }
 
 .home-title {
