eff206e849bdf798befba22e31d78e88c23cabc0
frzrbox <david@freezerbox.dev>
2019-11-25 14:34:39 -1000

    broke up scss into partials

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -24,7 +24,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import "@/scss/_variables";
+@import "@/scss/partials/_variables";
 
 #app-container {
   min-height: 100vh;
--- src/components/Question.vue
+++ src/components/Question.vue
@@ -12,7 +12,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import "@/scss/_variables";
+@import "@/scss/partials/_variables";
 
 .question {
   width: 90%;
