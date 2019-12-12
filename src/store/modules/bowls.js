db6c4251f20b6972866430eb58f033e544e0d75c
frzrbox <david@freezerbox.dev>
2019-12-12 00:25:54 -1000

    adds bowl result data to vuex store

--- src/store/modules/bowls.js
+++ src/store/modules/bowls.js
@@ -2,13 +2,48 @@ const state = {
     bowls: [
         {
             // not finished
-            name: 'Optimistic',
+            name: 'Hipster',
             refId: 0,
             // this is the title of what the topping is called
-            title: 'sunny side up egg, lime, cilantro, and emanating rays of light.',
+            title: 'Raku japanese bowl with wooden chopsticks.',
             // use this to remind you to the svg that will be output
-            type: 'sunny side up egg, lime, cilantro, and emanating rays of light.',
-            description: 'a ray of Sunshine Positive, bright, joyful'
+            type: 'Raku japanese bowl.',
+            description: 'wooden.'
+        },
+        {
+            name: 'Classy',
+            refId: 1,
+            title: 'Bowl with clean lines.',
+            type: 'Bowl with clean lines.',
+            description: 'minimalist and chic.'
+        },
+        {
+            name: 'Local',
+            refId: 2,
+            title: 'Chinese style bowl with bamboo chopsticks.',
+            type: 'Chinese style bowl with bamboo chopsticks.',
+            description: 'simple'
+        },
+        {
+            name: 'Eclectic',
+            refId: 3,
+            title: 'patterned bowl with jewel embedded bronze chopsticks',
+            type: 'patterned bowl with jewel embedded bronze chopsticks',
+            description: 'bohemian.'
+        },
+        {
+            name: 'Cute',
+            refId: 4,
+            title: 'cutely patterned bowl with character chopsticks.',
+            type: 'cutely patterned bowl with character chopsticks.',
+            description: 'cute, adorable and whimsical.'
+        },
+        {
+            name: 'Glamorous',
+            refId: 5,
+            title: 'crazy looking bowl that is brown.',
+            type: 'crazy looking bowl that is brown.',
+            description: 'weird looking.'
         }
     ]
 };
