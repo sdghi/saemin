3475d0e7cf9149dbd61b7d7b0fd5766241d957e9
frzrbox <david@freezerbox.dev>
2019-12-11 14:33:43 -1000

    adds all personality questions

--- src/store/modules/questions.js
+++ src/store/modules/questions.js
@@ -3,6 +3,7 @@ const state = {
     quizStarted: false,
     quizCompleted: false,
     questions: [
+        // PERSONALITY QUESTIONS
         {
             content: 'Pick a spirit animal',
             isAnswered: false,
@@ -102,6 +103,263 @@ const state = {
                 }
             ]
         },
+        {
+            content: 'Pick an experience',
+            isAnswered: false,
+            illustrationRef: 0,
+            ingredientRef: 0,
+            answers: [
+                {
+                    content: 'Concert',
+                    value: 0
+                },
+                {
+                    content: 'Game Night',
+                    value: 1
+                },
+                {
+                    content: 'Staycation',
+                    value: 3
+                },
+                {
+                    content: 'Burning Man',
+                    value: 4
+                },
+                {
+                    content: 'Try a new restaurant',
+                    value: 5
+                },
+                {
+                    content: 'A workshop, class or conference',
+                    value: 2
+                }
+            ]
+        },
+        {
+            content: 'In school group projects, I was often in the role of:',
+            isAnswered: false,
+            illustrationRef: 0,
+            ingredientRef: 0,
+            answers: [
+                {
+                    content: "Devil’s Advocate",
+                    value: 1
+                },
+                {
+                    content: 'Mastermind',
+                    value: 2
+                },
+                {
+                    content: "Illustrator",
+                    value: 4
+                },
+                {
+                    content: 'Presenter',
+                    value: 0
+                },
+                {
+                    content: 'Facilitator',
+                    value: 5
+                },
+                {
+                    content: 'Supporter',
+                    value: 3
+                }
+            ]
+        },
+        {
+            content: 'Of the following, which is most important to you?',
+            isAnswered: false,
+            illustrationRef: 0,
+            ingredientRef: 0,
+            answers: [
+                {
+                    content: 'Harmony',
+                    value: 2
+                },
+                {
+                    content: 'Efficiency',
+                    value: 1
+                },
+                {
+                    content: 'Current Events',
+                    value: 0
+                },
+                {
+                    content: 'Expression',
+                    value: 4
+                },
+                {
+                    content: 'Open Mindedness',
+                    value: 5
+                },
+                {
+                    content: 'Peace',
+                    value: 3
+                }
+            ]
+        },
+        {
+            content: 'Which best represents you?',
+            isAnswered: false,
+            illustrationRef: 0,
+            ingredientRef: 0,
+            answers: [
+                {
+                    content: 'The roof',
+                    value: 3
+                },
+                {
+                    content: 'The door',
+                    value: 5
+                },
+                {
+                    content: 'The window',
+                    value: 4
+                },
+                {
+                    content: 'The space',
+                    value: 5
+                },
+                {
+                    content: 'The living room',
+                    value: 0
+                },
+                {
+                    content: 'The pillars',
+                    value: 2
+                }
+            ]
+        },
+        {
+            content: 'You get a bonus day off. What do you do with it?',
+            isAnswered: false,
+            illustrationRef: 0,
+            ingredientRef: 0,
+            answers: [
+                {
+                    content: 'Catch up on some work',
+                    value: 2
+                },
+                {
+                    content: 'Indulge in a hobby',
+                    value: 5
+                },
+                {
+                    content: 'Dive into a book',
+                    value: 1
+                },
+                {
+                    content: 'Make plans to go out',
+                    value: 0
+                },
+                {
+                    content: 'Spend time with the fam',
+                    value: 3
+                },
+                {
+                    content: 'Do something spontaneous',
+                    value: 4
+                }
+            ]
+        },
+        {
+            content: 'Pick the character from The Office you relate to most',
+            isAnswered: false,
+            illustrationRef: 0,
+            ingredientRef: 0,
+            answers: [
+                {
+                    content: 'Michael Scott',
+                    value: 5
+                },
+                {
+                    content: 'Dwight Schrute',
+                    value: 1
+                },
+                {
+                    content: 'Phyllis Vance',
+                    value: 3
+                },
+                {
+                    content: 'Kelley Kapoor',
+                    value: 0
+                },
+                {
+                    content: 'Ryan Howard',
+                    value: 4
+                },
+                {
+                    content: 'Deangelo Jeremitrius Vickers',
+                    value: 2
+                }
+            ]
+        },
+        {
+            content: 'My Kryptonite:',
+            isAnswered: false,
+            illustrationRef: 0,
+            ingredientRef: 0,
+            answers: [
+                {
+                    content: 'Emotions',
+                    value: 1
+                },
+                {
+                    content: 'Rigid Rules & Institutions',
+                    value: 4
+                },
+                {
+                    content: 'Too Much Pressure',
+                    value: 3
+                },
+                {
+                    content: 'Unwanted Change',
+                    value: 0
+                },
+                {
+                    content: 'Micromanagement',
+                    value: 5
+                },
+                {
+                    content: 'Analysis Paralysis',
+                    value: 2
+                }
+            ]
+        },
+        {
+            content: 'Zombie apocalypse! Pick a thing',
+            isAnswered: false,
+            illustrationRef: 0,
+            ingredientRef: 0,
+            answers: [
+                {
+                    content: 'Armor',
+                    value: 3
+                },
+                {
+                    content: 'Crossbow',
+                    value: 2
+                },
+                {
+                    content: 'Cool outfit',
+                    value: 0
+                },
+                {
+                    content: 'Laboratory',
+                    value: 1
+                },
+                {
+                    content: 'Abandoned Mall',
+                    value: 4
+                },
+                {
+                    content: 'Car',
+                    value: 5
+                }
+            ]
+        },
+        // MOOD QUESTIONS
 
     ]
 
