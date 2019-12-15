6860454f5355defd0ff67d8c0abbc84f6c8d6775
frzrbox <david@freezerbox.dev>
2019-12-14 15:05:27 -1000

    fixes responsive scroll cta issue

--- src/components/ScrollDownCta.vue
+++ src/components/ScrollDownCta.vue
@@ -7,7 +7,17 @@
 <script>
 export default {
   name: "ScrollDownCta",
-  props: ["scrollCtaHeight"]
+  props: ["scrollCtaHeight", "quizStarted"],
+  mounted() {
+    // Will handle initial scroll down to first question when starting quiz
+    this.quizStarted &&
+      setTimeout(() => {
+        window.scrollTo({
+          top: this.$refs.scrollCta.offsetTop,
+          behavior: "smooth"
+        });
+      }, 1000);
+  }
 };
 </script>
 
